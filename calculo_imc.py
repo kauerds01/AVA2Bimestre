b9ccaf2b7d8027daefaa399a0112a80d99056634
DaviMarc <silvamarculinodavi@gmail.com>
2026-06-02 19:40:56 -0300

    Atualizar o calculo_imc.py

--- calculo_imc.py
+++ calculo_imc.py
@@ -2,3 +2,11 @@
 def calcular_imc(p, a):
     imc = p / (a ** 2)
     return imc
+#Davi da Silva Marculino: Parte 3--
+def gerar_aviso(status):
+    if status == "NORMAL":
+        return("Parabéns! Continue mantendo hábitos saudáveis.")
+    elif status == "ACIMA DO PESO":
+        return("Procure praticar atividades físicas e manter uma alimentação equilibrada.")
+    else:
+        return("Status Inválido.")
