6d659c64448aadc22ad8f608ddde24debed84bda
Sercrecy <caetanotiaguinho@gmail.com>
2026-06-03 09:47:55 -0300

    Update calculo_imc.py

--- calculo_imc.py
+++ calculo_imc.py
@@ -2,6 +2,12 @@
 def calcular_imc(p, a):
     imc = p / (a ** 2)
     return imc
+#Tiago Caetao Santos Silva Parte 2--
+def classificador(valor_imc):
+    if valor_imc < 25 
+        return "Peso na media"
+    else:
+        return "Acima da media"
 #Davi da Silva Marculino: Parte 3--
 def gerar_aviso(status):
     if status == "NORMAL":
