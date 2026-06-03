0a499d37668081181766e168d8146c9bf731219b
kauerds01 <rodrigueskaue087@gmail.com>
2026-06-03 14:29:08 +0000

    Versão_corrijida

--- calculo_imc.py
+++ calculo_imc.py
@@ -1,13 +1,13 @@
 #Victor Hugo Cardoso: Lógica Matemática
-def calcular_imc(peso, altura):
-    imc = peso / (altura ** 2)
+def calcular_imc(p, a):
+    imc = p / (a * 2)
     return imc
 #Tiago Caetao Santos Silva Parte 2--
 def classificador(valor_imc):
-    if valor_imc < 25: 
-        return "Peso na media"
+    if valor_imc <= 25: 
+        return "NORMAL"
     else:
-        return "Acima da media"
+        return "ACIMA DO PESO"
 #Davi da Silva Marculino: Parte 3--
 def gerar_aviso(status):
     if status == "NORMAL":
@@ -20,7 +20,10 @@ def gerar_aviso(status):
 print("Bem vindo ao seu avaliador de saúde pessoal 😁")
 print("Aqui iremos avaliar seu estado físico 💪🏻")
 peso = float(input("Qual é seu peso atual?"))
-altura = float(input("Qual é sua altura atual em cm?"))
+altura = float(input("Qual é sua altura atual em metros?"))
 imc_final = calcular_imc(peso, altura)
-status_final = classificador(valor_imc)
-aviso_final = gerar_aviso(status)
+status_final = classificador(imc_final)
+aviso_final = gerar_aviso(status_final)
+print(f"Seu imc é: {imc_final:.2f}")
+print(f"Sua situação atual é: {status_final}")
+print(f"feedback para você: {aviso_final}")
